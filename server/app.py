0ad21b11ee0276d056ae02e068418453e0114cc6
dkerubo <damariskerry@gmail.com>
2025-04-14 21:51:20 +0300

    update

--- server/app.py
+++ server/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def home():
-    return {"message": "Chama Backend Running"}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chama.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-if __name__ == "__main__":
+db = SQLAlchemy(app)
+
+# Import models here later
+
+if __name__ == '__main__':
     app.run(debug=True)
